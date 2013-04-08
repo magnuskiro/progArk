bb8b42f883d7f0b16cbdf8d6c1cdb9b1c4d14778
alex <janbremnes@gmail.com>
2013-04-08 14:30:23 +0200

    Testing out how to create new activities

--- src/no/ntnu/Battleship/Platform.java
+++ src/no/ntnu/Battleship/Platform.java
@@ -2,4 +2,15 @@ package no.ntnu.Battleship;
 
 public interface Platform {
 
+	void attack();
+	
+	void chooseWeapon();
+	
+	int getWeaponShots();
+	
+	int getShots();
+	
+	int[] getCoordinates();
+	
+	
 }
