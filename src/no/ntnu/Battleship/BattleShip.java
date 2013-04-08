89239e38b3c59e041a63cdabf0497e43abd0e0c2
Magnus Kirø <magnuskiro@gmail.com>
2013-04-08 10:38:32 +0200

    added res folder

--- src/no/ntnu/Battleship/BattleShip.java
+++ src/no/ntnu/Battleship/BattleShip.java
@@ -1,9 +1,13 @@
 package no.ntnu.Battleship;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 public class BattleShip extends Activity {
+
+    ProgressDialog progressDialog;
+
     /**
      * Called when the activity is first created.
      */
@@ -16,6 +20,7 @@ public class BattleShip extends Activity {
 
     // setup stuffs.
     private void init(){
+        progressDialog = new p
 
     }
 
