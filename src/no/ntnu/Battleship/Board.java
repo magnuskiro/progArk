49740f2268e07318d0e65e7d42e5b18d197cbb64
Raven <raven@mint>
2013-04-08 11:10:00 +0200

    Game changed

--- src/no/ntnu/Battleship/Board.java
+++ src/no/ntnu/Battleship/Board.java
@@ -1,5 +1,6 @@
 package no.ntnu.Battleship;
 
 public class Board {
-
+	private final int sizex = 10;
+	private final int sizey = 10;
 }
--- src/no/ntnu/Battleship/Game.java
+++ src/no/ntnu/Battleship/Game.java
@@ -1,21 +1,38 @@
 package no.ntnu.Battleship;
 
+import java.util.ArrayList;
+
 public class Game {
 	Board player1Board;
 	Board player2Board;
 	boolean p1Turn;
+	ArrayList<GameListener> listeners;
 	
 	public Game(Board p1, Board p2) {
 		this.player1Board = p1;
 		this.player2Board = p2;
-		p1Turn = true;
+		p1Turn = false;
+		listeners = new ArrayList<GameListener>();
 	}
 	
-	public Board getActive() {
+	public Board getAndSwitchActive() {
+		p1Turn = !p1Turn;
 		if (p1Turn) return player1Board;
 		return player2Board;
 	}
 	
+	public void addListener(GameListener listener) {
+		this.listeners.add(listener);
+	}
 	
+	public void removeListener(GameListener listener) {
+		this.listeners.remove(listener);
+	}
+	
+	public void gameChanged() {
+		for (GameListener listener:listeners) {
+			listener.fireGameChangedEvent();
+		}
+	}
 	
 }
