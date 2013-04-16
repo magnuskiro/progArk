df3957d18598402e571d5809d7a2dc16315de87b
Raven <raven@mint>
2013-04-16 18:22:30 +0200

    Board Logic DONE! (I think)

--- src/no/ntnu/Battleship/Board.java
+++ src/no/ntnu/Battleship/Board.java
@@ -115,13 +115,32 @@ public class Board {
 	
 	public void populate(ArrayList<Platform> newPlatforms) {
 		//populate platformGrid!
+		int[][] loc;
+		for(Platform plat:newPlatforms) {
+			loc = plat.getPosition();
+			for(int i = 0; i<loc.length; i++) {
+				platformGrid[loc[i][0]][loc[i][1]] = plat;
+			}
+		}
 		this.platforms = newPlatforms;
 	}
 	
 	public void attack(int[][] shots) {
-		
-		//TODO: input shots, calculate hits, misses, etc.
+		for(int i = 0; i<shots.length; i++) {
+			isShot[shots[i][0]][shots[i][1]] = true;
+			if(platformGrid[shots[i][0]][shots[i][1]] != null) {
+				platformGrid[shots[i][0]][shots[i][1]].attack(shots[i]);
+			}
+		}
 		myGame.gameChanged();
+	}
+	
+	public int getShots() {
+		int countShots = 0;
+		for(Platform plat:platforms) {
+			countShots = plat.getShots();
+		}
+		return countShots;
 	}
 	
 	public TileNum[][] getTiles() {
