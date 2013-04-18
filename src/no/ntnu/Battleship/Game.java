5484140a4412f9cc3109168098b8f27a10b17454
Håkon Hesselberg <hakon.hesselberg@gmail.com>
2013-04-18 06:46:40 +0200

    Jobbet en del med å få fram grafikk, kan printe ut et board i variabel størrelse, i tillegg til indeksering av nevnte brett. Brettet vil være nederst på skjermen, og skalert i forhold til bredden. For å vise noe på dette punktet må man starte GameActivity.java ved å skifte launch i manifestet. All grafikken ligger i en egen pakke.

--- src/no/ntnu/Battleship/Game.java
+++ src/no/ntnu/Battleship/Game.java
@@ -8,25 +8,31 @@ import android.util.Log;
 
 public class Game extends Activity {
 	private static final String TAG = "Battleship";
-	
+
 	public static final String KEY_SIZE = "no.ntnu.Battleship.size";
 	public static final int SIZE_SMALL = 0;
 	public static final int SIZE_MEDIUM = 1;
 	public static final int SIZE_LARGE = 2;
-	
+
 	Board player1Board;
 	Board player2Board;
 	int p1Shots;
 	int p2Shots;
 	boolean p1Turn;
+
+	// ######################
+	PlatformFactory platformFactory = new PlatformFactory();
+	DeviceFactory deviceFactory = new DeviceFactory();
+	// ######################
+
 	ArrayList<GameListener> listeners;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		Log.d(TAG, "onCreate");
-		
+
 		int size = 0;
 		switch (getIntent().getIntExtra(KEY_SIZE, SIZE_SMALL)) {
 		case 0:
@@ -41,21 +47,20 @@ public class Game extends Activity {
 		default:
 			break;
 		}
-		
+
 		player1Board = new Board(this, size);
 		player2Board = new Board(this, size);
-	
-//		TODO: activate a board viewer
-//		setContentView(player1Board);
-//		player1Board.requestFocus();
-	}
-	
 
-	
-	public Game(){
-		
+		// TODO: activate a board viewer
+		// setContentView(player1Board);
+		// player1Board.requestFocus();
+
+	}
+
+	public Game() {
+
 	};
-	
+
 	public Game(Board p1, Board p2, int p1InitShots, int p2InitShots) {
 		this.player1Board = p1;
 		this.player2Board = p2;
@@ -63,23 +68,31 @@ public class Game extends Activity {
 		p2Shots = p2InitShots;
 		p1Turn = false;
 		listeners = new ArrayList<GameListener>();
+
+		// ######################
+		player1Board.populate(platformFactory.createPlatforms());
+		player2Board.populate(platformFactory.createPlatforms());
+		// ######################
 	}
-	
+
 	/**
 	 * Set the current player to the next turn and get the board.
+	 * 
 	 * @return
 	 */
 	public Board getAndSwitchActive() {
 		p1Turn = !p1Turn;
-		if (p1Turn) return player2Board;
+		if (p1Turn)
+			return player2Board;
 		return player1Board;
 	}
-	
+
 	public int getShots() {
-		if (p1Turn) return p1Shots;
+		if (p1Turn)
+			return p1Shots;
 		return p2Shots;
 	}
-	
+
 	public void updateShots(int newCount) {
 		if (p1Turn) {
 			p2Shots = newCount;
@@ -87,20 +100,19 @@ public class Game extends Activity {
 			p1Shots = newCount;
 		}
 	}
-	
+
 	public void addListener(GameListener listener) {
 		this.listeners.add(listener);
 	}
-	
+
 	public void removeListener(GameListener listener) {
 		this.listeners.remove(listener);
 	}
-	
+
 	public void gameChanged() {
-		for (GameListener listener:listeners) {
+		for (GameListener listener : listeners) {
 			listener.fireGameChangedEvent();
 		}
 	}
-	
-}
 
+}
---
+++ src/no/ntnu/Battleship/graphics/BoardGraphics.java
@@ -0,0 +1,213 @@
+package no.ntnu.Battleship.graphics;
+
+import java.util.ArrayList;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Paint.Style;
+
+public class BoardGraphics {
+
+	private Paint paint;
+
+	private double tileSize;
+	private int boardSize;
+	private int screenWidth;
+	private int screenHeight;
+
+	private ArrayList<Rect> boardTiles;
+
+	public BoardGraphics(int boardSize, int screenWidth, int screenHeight) {
+
+		// this.tileSize = Math.floor(screenWidth / (boardSize + 2));
+		this.tileSize = screenWidth / (boardSize + 2);
+		this.boardSize = boardSize;
+		this.screenWidth = screenWidth;
+		this.screenHeight = screenHeight;
+
+		paint = new Paint();
+
+		boardTiles = createBoardTiles(boardSize, tileSize);
+	}
+
+	public void drawBoard(Canvas canvas) {
+
+		// Hvis partall kolonner må første tile i hver rad ha samme farge som
+		// siste tile i forrige rad
+		boolean evenNumberOfRows = false;
+		int columnCounter = 0;
+		if (boardSize % 2 == 0) {
+			evenNumberOfRows = true;
+		}
+
+		// Holder orden på at annenhver tile er grå/mørkegrå
+		boolean evenOrOdd = true;
+		// Setter paintstylen til fill
+		paint.setStyle(Style.FILL);
+		// Tegner hver tile
+		for (Rect tile : boardTiles) {
+			// Mer annenhver stuff
+			if (evenOrOdd) {
+				paint.setColor(Color.GRAY);
+			} else {
+				paint.setColor(Color.DKGRAY);
+			}
+			// Tilen tegnes
+			canvas.drawRect(tile, paint);
+			// Annenhvertelleren oppdateres
+			if (evenNumberOfRows && columnCounter == boardSize - 1) {
+				columnCounter = 0;
+			} else {
+				evenOrOdd = !evenOrOdd;
+				columnCounter++;
+			}
+		}
+	}
+
+	// public void drawIndexes(Canvas canvas) {
+	// paint.setStyle(Style.FILL);
+	// paint.setColor(Color.GRAY);
+	//
+	// int displacementFromTop = screenHeight - screenWidth + (int) tileSize;
+	//
+	// int displacementFromEdge = (int) (0);
+	//
+	// paint.setTextSize((float) tileSize-2);
+	//
+	// int topX;
+	// int topY;
+	//
+	// for (int i = 0; i < boardSize; i++) {
+	// // Koordinatene for bokstaver (rad)
+	// topX = (int) (i * tileSize + tileSize) + displacementFromEdge;
+	// topY = displacementFromTop + displacementFromEdge;
+	// // bruk charcode
+	// canvas.drawText("" + i, topX, topY, paint);
+	//
+	// // Koordinatene for siffer (kolonne)
+	// topX = 0 + displacementFromEdge;
+	// topY = (int) (i * tileSize + tileSize + displacementFromTop) +
+	// displacementFromEdge;
+	// canvas.drawText("" + i, topX, topY, paint);
+	// }
+	// }
+
+	public void drawIndexes(Canvas canvas) {
+		paint.setStyle(Style.FILL);
+		paint.setColor(Color.RED);
+
+		int displacementFromTop = screenHeight - screenWidth + (int) tileSize;
+
+		int displacementFromEdge = (int) (0);
+
+		paint.setTextSize((float) tileSize - 2);
+
+		int topX;
+		int topY;
+		int left;
+		int top;
+		int right;
+		int bot;
+
+		Rect drawWithin = new Rect();
+
+		for (int i = 0; i < boardSize; i++) {
+
+			// //rad
+			// left = (int) (i*tileSize + tileSize);
+			// top = displacementFromTop;
+			// right = (int) (left + tileSize);
+			// bot = (int) (top + tileSize);
+			// drawWithin.set(left, top, right, bot);
+			// paint.setColor(Color.GREEN);
+			// canvas.drawRect(drawWithin, paint);
+			// paint.setColor(Color.RED);
+			// canvas.drawText("" + i, drawWithin.left, drawWithin.top, paint);
+
+			// Koordinatene for bokstaver (rad)
+			topX = (int) (i * tileSize + tileSize) + displacementFromEdge;
+			topY = displacementFromTop + displacementFromEdge;
+			// bruk charcode
+			canvas.drawText("" + Character.valueOf((char) ('A' + i)), topX,
+					topY, paint);
+
+			// Koordinatene for siffer (kolonne)
+			topX = 0 + displacementFromEdge;
+			topY = (int) (i * tileSize + tileSize + displacementFromTop)
+					+ displacementFromEdge;
+			canvas.drawText("" + (i + 1), topX, topY, paint);
+		}
+	}
+
+	/*
+	 * int positionInRow = i % boardSize;
+	 * 
+	 * // Finner raden til tilen, aka topstreken, ikke midten eller under int
+	 * row = (int) (Math.floor(i / boardSize));
+	 * 
+	 * int displacementFromTop = screenHeight - screenWidth + (int)tileSize;
+	 * 
+	 * int left = (int) (positionInRow + positionInRow * tileSize); int top =
+	 * (int) (row + row * tileSize); int right = (int) (positionInRow +
+	 * positionInRow * tileSize + tileSize); int bot = (int) (row + row *
+	 * tileSize + tileSize);
+	 * 
+	 * 
+	 * 
+	 * paint.setColor(Color.RED); paint.setTextSize(10); c.drawText("Score: " +
+	 * topScore, topBarRect.left + 1, topBarRect.bottom - 1, paint);
+	 * c.drawText("Score: " + botScore, botBarRect.left + 1, botBarRect.bottom -
+	 * 1, paint);
+	 */
+
+	/**
+	 * Lager en arraylist med tiles
+	 * 
+	 * @param boardSize
+	 *            Størrelsen på brettet, viktig for å vite plasseringer i rader
+	 *            og kolonner
+	 * @param tileSize
+	 *            Størrelsen på siden til hver enkelt tile (de er firkantede)
+	 * @return En ny arraylist med tilse som rectangles, alle med hver sin x1,
+	 *         y1, x2, y2 kordinat ferdig sat
+	 */
+	private ArrayList<Rect> createBoardTiles(int boardSize, double tileSize) {
+		// Matrisen med alle tilsene som vi returnerer
+		ArrayList<Rect> tiles = new ArrayList<Rect>();
+
+		int displacementFromTop = screenHeight - screenWidth + (int) tileSize;
+
+		// Fyllingen av tiles
+		for (int i = 0; i < boardSize * boardSize; i++) {
+			// Lager ett nytt rect
+			Rect tile = new Rect();
+
+			// Finner posisjonen til tilen i raden, aka kolonnen dens (til
+			// venstre side, ikke midten eller høyre)
+			int positionInRow = i % boardSize;
+
+			// Finner raden til tilen, aka topstreken, ikke midten eller under
+			int row = (int) (Math.floor(i / boardSize));
+
+			int left = (int) (/* positionInRow + */positionInRow * tileSize);
+			int top = (int) (/* row + */row * tileSize);
+			int right = (int) (/* positionInRow + */left + tileSize);
+			int bot = (int) (/* row + */top + tileSize);
+
+			// Forskyver tiles så brettet kommer nederst på skjermen
+			left += tileSize;
+			top += displacementFromTop;
+			right += tileSize;
+			bot += displacementFromTop;
+
+			// Setter left, top, right, bottom
+			tile.set(left, top, right, bot);
+
+			// Legger tilen til arraylisten som returneres
+			tiles.add(tile);
+		}
+		return tiles;
+	}
+}
